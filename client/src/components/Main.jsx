91f5a7ee5e779700198255485c5df0f9d4749a78
tomy4649 <tkaaynakiyoshi@gmail.com>
2022-12-09 23:09:52 +0900

    fix overall

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -18,32 +18,40 @@ const TopPage = () => {
         <div className="top-msg2">Let's start investing.</div>
       </div>
       <div className="card-list">
+        <Link to="/stockdetail" className="card-link">
+          <StockCard
+              className="card"
+              image={Live}
+              name="株式会社Queen Record"
+              desc="10周年ライブが2034年2月に開催決定！バーチャルアーティスト事業の先駆け。"
+          />
+        </Link>
         <StockCard
-            image={Live}
-            name="株式会社Queen Record"
-            desc="10周年ライブが2034年2月に開催決定！バーチャルアーティスト事業の先駆け。"
-        />
-        <StockCard
+          className="card"
           image={Shibuya}
           name="株式会社Angel Home"
           desc="従来の不動産にとらわれない新しい価値を創造。エンジェルグループ新規ベンチャー。"
         />
         <StockCard
+          className="card"
           image={Forest}
           name="東京七釜戸株式会社"
           desc="新奥地区初の再開発始動！次世代自然連携型スマートシティの未来。"
         />
         <StockCard
+          className="card"
           image={Cyber}
           name="デジタルPhoenix株式会社"
           desc="半永久不滅な自律分散型クラウド。あなたの財産を守ります。"
         />
         <StockCard
+          className="card"
           image={Tsumiki}
           name="株式会社八田教育センター"
           desc="学習用アプリ「八田式チャレンジ」正式リリース！人気講師多数在籍！"
         />
         <StockCard
+          className="card"
           image={Airplane}
           name="S&K株式会社"
           desc="航空業界にDXを。半自動型運航スケジュール作成システム。"
@@ -66,10 +74,10 @@ const MyStock = () => {
       <h1 class="mystock-title">保有株式一覧</h1>
       <div className="card-list">
         <StockCard
-          image={Shibuya}
-          name="株式会社Angel Home"
-          desc="従来の不動産にとらわれない新しい価値を創造。エンジェルグループ新規ベンチャー。"
-          price={0.250}
+            className="card"
+            image={Live}
+            name="株式会社Queen Record"
+            desc="10周年ライブが2034年2月に開催決定！バーチャルアーティスト事業の先駆け。"
         />
       </div>
     </div>
@@ -84,7 +92,7 @@ export const Main = () => {
           <div className="side-content">
             <ul>
               <li><Link to="/" className="link">＞ 銘柄一覧</Link></li>
-              <li><Link to="/stockdetail" className="link">＞ 保有株式一覧</Link></li>
+              <li><Link to="/mystock" className="link">＞ 保有株式一覧</Link></li>
               <li>＞ 取引履歴</li>
               <li>＞ ニュース</li>
               <li>＞ 事業者の方へ</li>
@@ -94,6 +102,7 @@ export const Main = () => {
         </div>
         <Switch>
           <Route exact path="/" component={TopPage} />
+          <Route path="/mystock" component={MyStock} />
           <Route path="/stockdetail" component={StockDetail} />
         </Switch>
       </div>
--- client/src/components/StockDetail.jsx
+++ client/src/components/StockDetail.jsx
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import Live from "../assets/Live/jpg";
+import Live from "../assets/Live.jpg";
+import Crown from "../assets/Crown.png";
+import "./StockDetail.css";
 
 export const StockDetail = () => {
     return (
         <div>
             <img src={Live} className="top-img" />
-            <div className="icon"></div>
+            <img src={Crown} className="icon" />
             <h2 className="name">株式会社Queen Record</h2>
-            <h3 className="desc">登録日 Aug 2022   チェーン Ethereum   時価 0.01 ETH</h3>
-            <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-            <Button variant="contained">購入する</Button>
+            <h3>登録日 <span>Aug 2022</span>   チェーン <span>Ethereum</span>   時価 <span>0.01 ETH</span></h3>
+            <h3 className="desc">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h3>
+            <div>
+                <Button variant="contained" className="button">購入する</Button>
+            </div>
         </div>
     );    
   }
