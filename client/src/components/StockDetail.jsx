0b33e62346c51c5d4d7d0e1e2953a60d75fcd8ed
thous210 <thousdev@gmail.com>
2022-12-10 00:04:58 +0900

    [upd] UI

--- client/src/components/StockDetail.jsx
+++ client/src/components/StockDetail.jsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Live from "../assets/Live.jpg";
-import Crown from "../assets/Crown.png";
+import Crown from "../assets/Live.jpg";
 import "./StockDetail.css";
 
 export const StockDetail = () => {
     return (
         <div>
             <img src={Live} className="top-img" />
-            <img src={Crown} className="icon" />
-            <h2 className="name">株式会社Queen Record</h2>
-            <h3>登録日 <span>Aug 2022</span>   チェーン <span>Ethereum</span>   時価 <span>0.01 ETH</span></h3>
-            <h3 className="desc">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h3>
-            <div>
-                <Button variant="contained" className="button">購入する</Button>
+            <img src={Crown} alt="王冠" className="icon" />
+            <div className="content">
+                <h2 className="name">株式会社Queen Record</h2>
+                <h3>登録日 <span>Aug 2022</span>   チェーン <span>Ethereum</span>   時価 <span>0.01 ETH</span></h3>
+                <h3 className="desc">10周年ライブが2034年2月に開催決定！バーチャルアーティスト事業の先駆け。</h3>
+                <div  className="button">
+                    <Button variant="contained" size="large">購入する</Button>
+                </div>
             </div>
         </div>
-    );    
+    );
   }
--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,14 +1,14 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit"
-// import { logo } from "../../assets/Logo.png"
+import Logo from "../../assets/Logo.png"
 import "./Header.css"
 
 export const Header = () => {
   return (
     <header>
-      <div className="logo">Crypto Stock</div>
+      <img src={Logo} className="logo" />
       <div className="search">
         <div className="search-box">
-          <p>　　　銘柄を探す</p>
+          <p>　銘柄を探す</p>
         </div>
         <div className="search-button">
           <p>検索</p>
